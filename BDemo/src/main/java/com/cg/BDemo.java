f146eda6b12d8020424e21ac8472525977a4fe57
javija <javija@DIN73003057.corp.capgemini.com>
2018-11-24 09:22:32 +0530

    commit

---
+++ BDemo/src/main/java/com/cg/BDemo.java
@@ -0,0 +1,16 @@
+package com.cg;
+
+public class BDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!!!!!");
+		
+		getMessage();
+
+	}
+
+	private static void getMessage() {
+System.out.println("Hi!");		
+	}
+
+}
